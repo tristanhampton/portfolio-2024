c832becbd35c78e10581590275eef64b97a1774d
Tristan Hampton <tristanhampton@icloud.com>
2024-09-16 15:59:57 -0600

    Updating prod webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,13 +1,3 @@
 const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
-const common = require('./webpack.config.common.js'); module.exports = merge(common, {
-  // Enable minification and tree-shaking
-  mode: 'production',
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-      }),
-    ],
-  },
-});
+const common = require('./webpack.config.common.js');
